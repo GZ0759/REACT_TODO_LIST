77d768618bf5aa14ef787ae0f09882680a02b5d1
Muses <1239085033@qq.com>
2021-12-27 10:24:37 +0800

    add grid and fix less

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useCallback } from 'react';
-import './App.scss';
-import { List } from 'antd';
+import { Row, Col, List } from 'antd';
 import ListHead from './components/ListHead';
 import ListInput from './components/ListInput';
 import ListItem from './components/ListItem';
+import './App.scss';
 
 function App() {
   const [showInput, setShowInput] = useState(false);
@@ -20,21 +20,28 @@ function App() {
     setList((list) => [...list, item]);
     setShowInput(false);
   }, []);
+
   return (
     <div className="App">
-      <ListHead openInput={openInput}></ListHead>
-      <ListInput isInputShow={showInput} addItem={addItem}></ListInput>
-      <div className="content">
-        <List
-          dataSource={list}
-          locale={{ emptyText: '这里是空空如也' }}
-          renderItem={(item) => (
-            <List.Item key={item.id}>
-              <ListItem data={item} />
-            </List.Item>
-          )}
-        />
-      </div>
+      <Row>
+        <Col xs={0} sm={0} md={4} lg={6} xl={7}></Col>
+        <Col xs={24} sm={24} md={16} lg={12} xl={10}>
+          <ListHead openInput={openInput}></ListHead>
+          <ListInput isInputShow={showInput} addItem={addItem}></ListInput>
+          <div className="content">
+            <List
+              dataSource={list}
+              locale={{ emptyText: '这里是空空如也' }}
+              renderItem={(item) => (
+                <List.Item key={item.id}>
+                  <ListItem data={item} />
+                </List.Item>
+              )}
+            />
+          </div>
+        </Col>
+        <Col xs={0} sm={0} md={4} lg={6} xl={7}></Col>
+      </Row>
     </div>
   );
 }
