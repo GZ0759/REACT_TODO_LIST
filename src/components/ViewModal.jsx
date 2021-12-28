e9e673a44d6bc5ca71b7e17b51a0a47299adb89f
Muses <1239085033@qq.com>
2021-12-28 09:43:05 +0800

    fix error and update modal

--- src/components/ViewModal.jsx
+++ src/components/ViewModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal } from 'antd';
+import { Modal, Button } from 'antd';
 import './ViewModal.scss';
 
 const ViewModal = (props) => {
@@ -7,12 +7,7 @@ const ViewModal = (props) => {
 
   return (
     <>
-      <Modal
-        title="查看待办事项"
-        visible={show}
-        onOk={closeModal}
-        onCancel={closeModal}
-      >
+      <Modal title="查看待办事项" visible={show} footer={null}>
         <p>
           <span>时间：</span>
           <span>{content.id}</span>
@@ -25,6 +20,11 @@ const ViewModal = (props) => {
           <span>状态：</span>
           <span>{content.completed ? '完成' : '未完成'}</span>
         </p>
+        <div className="btn">
+          <Button type="primary" onClick={closeModal}>
+            确定
+          </Button>
+        </div>
       </Modal>
     </>
   );
